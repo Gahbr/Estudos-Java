f3c6c48ab23daeed619a9b74c296bea23a977cb3
Gabriel Ribeiro <80289718+Gahbr@users.noreply.github.com>
2023-03-31 16:50:58 -0300

    feat: estrutura de dados - pilha

---
+++ src/DataEstructure/Pilha.java
@@ -0,0 +1,46 @@
+package DataEstructure;
+
+public class Pilha {
+    private No refNoEntradaPilha;
+
+    public Pilha() {
+        this.refNoEntradaPilha = null;
+    }
+    public void push(No novoNo){
+        No refAuxiliar = refNoEntradaPilha;
+        refNoEntradaPilha = novoNo;
+        refNoEntradaPilha.setRefNo(refAuxiliar);
+    }
+    public No pop(){
+        if(!this.isEmpty()){
+            No noPopped = refNoEntradaPilha;
+            refNoEntradaPilha = refNoEntradaPilha.getRefNo();
+        }
+        return null;
+    }
+    public No top(){
+        return refNoEntradaPilha;
+    }
+    public boolean isEmpty(){
+        return refNoEntradaPilha == null ? true : false;
+    }
+
+    @Override
+    public String toString() {
+        String stringRetorno = "-------\n";
+        stringRetorno += "    Pilha\n";
+        No noAuxiliar = refNoEntradaPilha;
+         stringRetorno += "-------\n";
+
+        while(true){
+            if(noAuxiliar !=null){
+                stringRetorno+= "[No{dado=" + noAuxiliar.getDado() + "}]\n";
+                noAuxiliar = noAuxiliar.getRefNo();
+            }else {
+                break;
+            }
+        }
+        stringRetorno += "========\n";
+        return stringRetorno;
+    }
+}
